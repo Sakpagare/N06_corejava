7504a5732b6754513d9c456b53231033fffb5cc1
ganes <ganes@LAPTOP-9CN0131S.mshome.net>
2023-07-20 21:56:05 +0530

    firstcommit

---
+++ N06_corejava/src/com/met/introduction/ComplierInput.java
@@ -0,0 +1,14 @@
+package com.met.introduction;
+import java.util.Scanner;
+
+public class ComplierInput {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the string: ");
+		String strl=s.nextLine();
+		System.out.println("String is: "+ strl);
+
+	}
+
+}
